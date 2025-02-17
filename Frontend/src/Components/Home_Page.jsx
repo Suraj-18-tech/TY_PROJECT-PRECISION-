704f9e29ef38e6422dfe3742f776cd6f95029fac
Suraj Honmane <surajhonmane18@gmail.com>
2025-02-18 00:53:11 +0530

    done

---
+++ Frontend/src/Components/Home_Page.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './Navbar'
+import Footer from './Footer'
+
+const Home_Page = () => {
+  return (
+    <div>
+        <Navbar/>
+        This is home page
+        <Footer/>
+    </div>
+  )
+}
+
+export default Home_Page
